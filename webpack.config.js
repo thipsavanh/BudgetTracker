6848c3013695b9016d8346f49ec9cb35fe483b57
thip <thipsavanh@gmail.com>
2020-05-05 22:54:14 -0500

    added webpack and service worker

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./assets/js/index.js",
+    
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Budget Tracker App",
+      short_name: "Budget Tracker",
+      description: "An application that allows you to to add expenses and deposits to their budget with or without a connection.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("assets/images/icons/icon-192x192.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ]
+};
+
+module.exports = config;
